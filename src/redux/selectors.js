96aad7344a021f25c72a2a1e2e03c8fa3bdb4478
Sujal <sujaltonge@gmail.com>
2026-09-04 22:49:04 +0530

    Add Redux selectors for cart data

---
+++ src/redux/selectors.js
@@ -0,0 +1,13 @@
+export const selectCartItems = (state) => state.cart.items;
+
+export const selectCartCount = (state) =>
+  state.cart.items.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+
+export const selectCartTotal = (state) =>
+  state.cart.items.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
