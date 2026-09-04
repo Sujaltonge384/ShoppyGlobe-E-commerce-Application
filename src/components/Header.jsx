380792f4c23bdc609588cd7c030149571c013ed1
Sujal <sujaltonge@gmail.com>
2026-09-04 22:51:00 +0530

    Create Header navigation and cart indicator

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectCartCount } from "../redux/selectors";
+
+function Header() {
+  const cartCount = useSelector(selectCartCount);
+
+  return (
+    <header className="header">
+      <div className="header-container">
+        <Link to="/" className="logo">
+          ShoppyGlobe
+        </Link>
+
+        <nav className="nav">
+          <Link to="/">Home</Link>
+
+          <Link to="/cart" className="cart-link">
+            🛒 Cart
+            <span className="cart-count">{cartCount}</span>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
